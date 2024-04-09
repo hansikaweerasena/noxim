4f8c0ae03f307fa7bb1ca0858a2e5bd8baa7c222
hansikaweerasena <hansikam94@gmail.com>
2024-04-09 18:36:24 -0400

    update collect data script

--- bin/collect_data.sh
+++ bin/collect_data.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-benchmarks=("FFT" "FMM" "LU" "BARNES" "OCEAN" "RADIX" "black")
-
-benchmarks=("FFT" "OCEAN" "RADIX")
+benchmarks=("FFT" "OCEAN" "RADIX" "FMM" "LU" "BARNES" "black")
 
 # Loop through the array and run the command for each benchmark
 for benchmark in "${benchmarks[@]}"; do
@@ -16,7 +14,8 @@ for benchmark in "${benchmarks[@]}"; do
     # Inner loop for executing the command with varying traffic trace values
     for offset in {0..63}; do
         echo "Running ${benchmark} with traffic trace 1 ${offset}..."
-        ./noxim -config ../config_examples/default_config_wireless_only_64.yaml -traffic trace 1 ${offset} > "wireless/${benchmark}/${offset}.txt"
+#        ./noxim -config ../config_examples/default_config_wireless_only_64.yaml -traffic trace 1 ${offset} > "wireless/${benchmark}/${offset}.txt"
+        ./noxim -config ../config_examples/default_config_wireless_64_16.yaml -traffic trace 1 ${offset} > "wireless/${benchmark}/${offset}.txt"
     done
 
     # Revert the file name back to the original
